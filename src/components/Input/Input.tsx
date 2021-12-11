76c9d7ee519a711940ea27cd57df83b3ab4a48fd
irfan-RapidzzSolutions <mohammadirfan@rapidzzsolutions.com>
2021-12-11 23:58:58 +0500

    Input component component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import styles from './input.module.scss';
 
 const Input = forwardRef(({
@@ -7,10 +7,15 @@ const Input = forwardRef(({
     type,
     ...rest
 }: any, ref) => {
+    const [value, setValue] = useState('');
+    const [error] = useState('');
     return (
         <div className={styles.inputContainer}>
-            <input type="text" className={styles.inputText} required />
-            <span className={styles.floatingLabel}>{label}</span>
+            <input type={type} className={error ? `${styles.error}` : ''} value={value} onChange={(e) => setValue(e.target.value)} />
+            <label className={`${value && `${styles.filled}`}${error ? `${styles.error}` : '' }`} htmlFor={label}>
+                Email Address
+            </label>
+            {error && <span className={styles.error}>Email must be formatted correctly.</span>}
         </div>
     )
 })
